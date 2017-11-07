37dcde313a0d1a95497efdbd4519d990d0ee2ca9
Tyler Heucke <tyler.heucke@dell.com>
2017-11-07 10:25:01 -0800

    Update debian mirror
    Change-Id: Ie993808e05a0f639ed75f795c19be2c04b74fe12

--- inst-hooks/98-set-apt-sources.postinst.sh
+++ inst-hooks/98-set-apt-sources.postinst.sh
@@ -4,14 +4,14 @@
 
 mkdir -p /etc/apt
 
-echo 'deb http://ftp.debian.org/debian/ jessie main contrib non-free' > /etc/apt/sources.list
-echo 'deb-src http://ftp.debian.org/debian/ jessie main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://httpredir.debian.org/debian/ jessie main contrib non-free' > /etc/apt/sources.list
+echo 'deb-src http://httpredir.debian.org/debian/ jessie main contrib non-free' >> /etc/apt/sources.list
 
-echo 'deb http://ftp.debian.org/debian/ jessie-backports main contrib non-free' >> /etc/apt/sources.list
-echo 'deb-src http://ftp.debian.org/debian/ jessie-backports main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://httpredir.debian.org/debian/ jessie-backports main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://httpredir.debian.org/debian/ jessie-backports main contrib non-free' >> /etc/apt/sources.list
 
-echo 'deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free' >> /etc/apt/sources.list
-echo 'deb-src http://ftp.debian.org/debian/ jessie-updates main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://httpredir.debian.org/debian/ jessie-updates main contrib non-free' >> /etc/apt/sources.list
+echo 'deb-src http://httpredir.debian.org/debian/ jessie-updates main contrib non-free' >> /etc/apt/sources.list
 
 echo 'deb http://security.debian.org/ jessie/updates main contrib non-free' >> /etc/apt/sources.list
 echo 'deb-src http://security.debian.org/ jessie/updates main contrib non-free' >> /etc/apt/sources.list
