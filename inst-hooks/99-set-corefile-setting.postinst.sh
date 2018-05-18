797dd0d395ba8eb2e660369c9a991f6f09ef617f
Garrick He <garrick_he@dell.com>
2018-05-17 20:21:30 -0700

    Enable corefile generation
    * Add postinst script to enable corefile generation for all users
    * Update XML file so postinst script gets executed during installation
    
    Change-Id: I9ee667d99b47ee8689746216c99c8797820ca2aa
    Signed-off-by: Garrick He <garrick_he@dell.com>

---
+++ inst-hooks/99-set-corefile-setting.postinst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This postinst script sets the corefile settings
+
+mkdir -p /var/cores
+echo "#Kernel corefile pattern" >> /etc/sysctl.conf
+echo "kernel.core_pattern = /var/cores/core.%e.%p" >> /etc/sysctl.conf
+echo "*   hard   core   unlimited" >> /etc/security/limits.conf
+echo "*   soft   core   unlimited" >> /etc/security/limits.conf
