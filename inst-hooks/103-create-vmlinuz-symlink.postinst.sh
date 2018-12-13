d24f5e20ee5c6bbb326ede769c04bec37bd983d1
Garrick He <garrick_he@dell.com>
2018-12-13 15:34:57 -0800

    fix: Remove unneeded postinst hook
    Remove the postinst hook that updates vmlinuz and initrd symlinks.
    
    Signed-off-by: Garrick He <garrick_he@dell.com>

--- inst-hooks/103-create-vmlinuz-symlink.postinst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This postinst creates the missing vmlinuz and initrd.img symlinks
-
-ln -sf /boot/vmlinuz-4.9.110-opx /vmlinuz
-ln -sf /boot/vmlinuz-4.9.110-opx /vmlinuz.old
-ln -sf /boot/initrd.img-4.9.110-opx /initrd.img
-ln -sf /boot/initrd.img-4.9.110-opx /initrd.img.old
-
